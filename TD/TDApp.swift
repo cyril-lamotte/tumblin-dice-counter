259789c6eea834c4b9d9f61c1527efe2484c4daf
Cyril Lamotte <lamotte.cyril@gmail.com>
2022-10-10 21:20:23 +0200

    Initial Commit

---
+++ TD/TDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDApp.swift
+//  TD
+//
+//  Created by Lamotte Cyril on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
